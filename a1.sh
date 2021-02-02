dd0689d54fa75fbca810603627b76e938a4f8506
Ankit <43137695+andalike@users.noreply.github.com>
2021-02-02 19:54:13 +0530

    Update a1.sh

--- a1.sh
+++ a1.sh
@@ -5,7 +5,7 @@ rm -rf terraform_0.14.5_linux_amd64.zip
 wget https://releases.hashicorp.com/terraform/0.14.5/terraform_0.14.5_linux_amd64.zip
 unzip terraform_0.14.5_linux_amd64.zip
 sudo mv terraform /usr/local/bin/
-echo "Check-1"
+echo "Changes sn111 to sn1"
 echo "Installed Terraform"
 rm -rf terraform_0.14.5_linux_amd64.zip
 
